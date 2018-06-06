0a30d5f89215017d3aa1d4d351584f31dc9a88a8
Blockost <simon.espigole@gmail.com>
2018-06-06 18:13:40 +0100

    Created first components + routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Custom modules
+import { RoutingModule } from './modules/routing/routing.module';
+
+// Custom components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [BrowserModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/routing/routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+
+// Import components
+import { HomeComponent } from '../../pages/home/home.component';
+import { PageNotFoundComponent } from '../../pages/page-not-found/page-not-found.component';
+
+// Add routes to components here (order matters) !
+const ROUTES = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**', // fallback route (no match)
+    redirectTo: '/404'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule],
+  declarations: []
+})
+export class RoutingModule {}
