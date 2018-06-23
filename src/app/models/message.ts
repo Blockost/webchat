fd68a0b4a475f7469eb7e27a17a5653824d851e9
ESPIGOLÉ Simon <simon.espigole@gmail.com>
2018-06-23 15:29:29 -0400

    Added JSDoc + message model

---
+++ src/app/models/message.ts
@@ -0,0 +1,7 @@
+export class Message {
+  constructor(
+    private from: string,
+    private to: string,
+    private content: string
+  ) {}
+}
--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
+/**
+ * Service to pass messages between components.
+ */
 @Injectable()
 export class MessageService {
   private messageSource = new Subject<string>();
   messageObservable = this.messageSource.asObservable();
 
   sendMessage(message: string) {
-    console.log('message received by the service');
     this.messageSource.next(message);
   }
 }
